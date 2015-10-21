5ec0e4ddf45aca13164915acd570baa66a7fcda2
saferthanhouses <joey_oliver@hotmail.com>
2015-10-21 15:42:22 -0400

    to signup and login controllers

--- browser/app/login/login.state.js
+++ browser/app/login/login.state.js
@@ -3,6 +3,14 @@
 app.config(function ($stateProvider) {
 	$stateProvider.state('login', {
 		url: '/login',
-		templateUrl: '/browser/app/login/login.html'
+		templateUrl: '/browser/app/login/login.html',
+		controller: "LoginCtrl"
 	});
 });
+
+app.controller('LoginCtrl', function($scope) {
+	
+	$scope.loginSubmit = function() {
+		console.log($scope.email, $scope.password);
+	}
+})
--- browser/app/signup/signup.state.js
+++ browser/app/signup/signup.state.js
@@ -3,6 +3,14 @@
 app.config(function ($stateProvider) {
 	$stateProvider.state('signup', {
 		url: '/signup',
-		templateUrl: '/browser/app/signup/signup.html'
+		templateUrl: '/browser/app/signup/signup.html',
+		controller: 'SignupCtrl'
 	});
 });
+
+app.controller('SignupCtrl', function($scope) {
+	$scope.signupSubmit = function() {
+		console.log($scope.email, $scope.password);
+
+	}
+})
